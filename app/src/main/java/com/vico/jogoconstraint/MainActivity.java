3efd13e3ba5bd5ef2016bf02c833e0a1beafb877
Vico <vpontin93@gmail.com>
2021-04-05 22:01:12 -0300

    Exercício parcialmente feito

--- app/src/main/java/com/vico/jogoconstraint/MainActivity.java
+++ app/src/main/java/com/vico/jogoconstraint/MainActivity.java
@@ -2,13 +2,35 @@ package com.vico.jogoconstraint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    ImageView btnJogoDaVelha;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnJogoDaVelha = findViewById(R.id.btnJogoDaVelha);
+
+        btnJogoDaVelha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirJogoDaVelha();
+            }
+        });
+    }
+
+    private void abrirJogoDaVelha()
+    {
+        Intent janelaVelha = new Intent(this, JogoVelha.class);
+        startActivity(janelaVelha);
+        finish();
     }
 }
