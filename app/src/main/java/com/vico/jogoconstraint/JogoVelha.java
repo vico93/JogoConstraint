faca2dee1174ce4844051c87e57781df6fbfce4e
Vico <vpontin93@gmail.com>
2021-04-05 21:45:03 -0300

    Adicionada tela do Jogo da Velha
    Falta o código

---
+++ app/src/main/java/com/vico/jogoconstraint/JogoVelha.java
@@ -0,0 +1,14 @@
+package com.vico.jogoconstraint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JogoVelha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo_velha);
+    }
+}
